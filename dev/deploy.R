e7ca1e3d7ad8b64abf29afbe0ae4c0f68b212522
Juliette Engelaere-Lefebvre <juliette.engelaere@developpement-durable.gouv.fr>
2023-07-26 16:57:45 +0200

    first try :baby:

---
+++ dev/deploy.R
@@ -0,0 +1,28 @@
+message("--- Add packages ---\n")
+
+install.packages("rsconnect")
+install.packages("shiny")
+
+message("--- Add server ---\n")
+
+rsconnect::setAccountInfo(
+  name = Sys.getenv("SHINYAPPS_NAME"),
+  token = Sys.getenv("SHINYAPPS_TOKEN"),
+  secret = Sys.getenv("SHINYAPPS_SECRET")
+)
+
+message("--- Deploying app ---\n")
+appFiles <- "app.R"
+
+# options(rsconnect.packrat = TRUE)
+# options(rsconnect.http.trace = TRUE)
+# options(rsconnect.check.certificate = FALSE)
+
+rsconnect::deployApp(
+  appDir = ".",
+  account = Sys.getenv("SHINYAPPS_NAME"),
+  appFiles = appFiles,
+  appName = Sys.getenv("NAME_OF_APP"),
+  lint = FALSE,
+  forceUpdate = TRUE
+)
